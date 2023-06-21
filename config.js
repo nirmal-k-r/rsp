57e62eda611dc556a4233e24a8e934e0698632f2
Nirmal <Nirmal@admin.com>
2023-06-21 11:15:55 +0400

    final

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ try {
    
     var config = {}
     // config.db = mongojs('mongodb://admin:12345@localhost:27017/myedu?authSource=admin');
-    config.db = mongojs('mongodb://localhost:27017/RSP');
+    // config.db = mongojs('mongodb://localhost:27017/RSP');
+    config.db=mongojs('mongodb+srv://jsuser:Di12345@cluster0.zb8r5jj.mongodb.net/?retryWrites=true&w=majority')
     console.log(`Database connection successful`);
 
 
